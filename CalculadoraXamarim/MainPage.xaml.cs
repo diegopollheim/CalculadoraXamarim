ab11b362ba81157ab54aa618e0f28a38ffb485fe
Diego Pollheim <diegopollheim@gmail.com>
2022-11-17 20:59:39 -0300

    captura dos numeros iniciada

--- CalculadoraXamarim/MainPage.xaml.cs
+++ CalculadoraXamarim/MainPage.xaml.cs
@@ -10,9 +10,54 @@ namespace CalculadoraXamarim
 {
     public partial class MainPage : ContentPage
     {
+
+        int currentState = 1;
+        string mathOperator;
+        double firstNumber, secondNumber;
+
+
+
         public MainPage()
         {
             InitializeComponent();
         }
+        private void OnSelectNumber(object sender, EventArgs e)
+        {
+            Button btn = (Button)sender;
+            string pressed = btn.Text;
+
+            if (this.resultText.Text == "0" || currentState < 0)
+            {
+                this.resultText.Text = "";
+                if (currentState < 0)
+                    currentState *= -1;
+            }
+
+            this.resultText.Text += pressed;
+
+            double number;
+            if (double.TryParse(this.resultText.Text, out number))
+            {
+                this.resultText.Text = number.ToString("N0");
+                if (currentState == 1)
+                {
+                    firstNumber= number;
+                }
+                else
+                {
+                    secondNumber= number;
+                }
+            }
+
+        }
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            firstNumber = 0;
+            secondNumber = 0;
+            currentState = 1;
+            this.resultText.Text = "0";
+        }
+
+
     }
 }
